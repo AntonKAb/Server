f8511bf76a06480f7ebed087b6deb44e3444eb32
AntonKAb <anton_10_1999@mail.ru>
2019-11-19 04:20:51 +0700

    update v2.1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import asyncio
 
 class ClientServerProtocol(asyncio.Protocol):
     def __init__(self):
-        self.metrcics_list = {}
+        self.metrics_list = {}
 
     def connection_made(self, transport):
         self.transport = transport
@@ -12,43 +12,40 @@ class ClientServerProtocol(asyncio.Protocol):
         self.metrics_processing(data)
 
     def metrics_processing(self, data):
-        try:
-            data_ = str(data.decode()).split()
-            if data_[0] is 'put':
-                self.transport.write(self.put_metric(data_).encode())
-            if data_[0] is 'get':
-                self.transport.write(self.get_metric(data_).encode())
-            if data_[0] is not 'put' or 'get':
-                self.transport.write('error\nwrong command\n\n'.encode())
-        except TimeoutError:
-            self.transport.write('time\nis out\n\n'.encode())
+        data_ = data.decode().split()
+        if data_[0] is 'put':
+            self.transport.write(self.put_metric(data_).encode())
+        elif data_[0] is 'get':
+            self.transport.write(self.get_metric(data_).encode())
+        elif data_[0] != 'put' and data_[0] != 'get':
+            self.transport.write('error\nwrong command\n\n'.encode())
 
     def put_metric(self, data_got):
-        if data_got[1] not in self.metrcics_list.keys():
-            self.metrcics_list[data_got[1]] = [tuple([data_got[2], data_got[3]])]
-            return 'written\n\n'
+        if data_got[1] not in self.metrics_list.keys():
+            self.metrics_list[data_got[1]] = [tuple([data_got[2], data_got[3]])]
+            return 'ok\n\n'
         else:
-            if self.metrcics_list[data_got[1]] == [tuple([data_got[2], data_got[3]])] or \
-                    tuple([data_got[2], data_got[3]]) in self.metrcics_list[data_got[1]]:
-                return 'already written\n\n'
+            if self.metrics_list[data_got[1]] == [tuple([data_got[2], data_got[3]])] or \
+                    tuple([data_got[2], data_got[3]]) in self.metrics_list[data_got[1]]:
+                return 'ok\n\n'
             else:
-                self.metrcics_list[data_got[1]].append(tuple([data_got[2], data_got[3]]))
-                return 'written\n\n'
+                self.metrics_list[data_got[1]].append(tuple([data_got[2], data_got[3]]))
+                return 'ok\n\n'
 
     def get_metric(self, data_send):
         if data_send[1] == '':
             return 'error\nwrong command\n\n'
-        if data_send[1] is '*':
+        elif data_send[1] is '*':
             server_answer = 'ok\n'
-            for key in self.metrcics_list:
-                for value in self.metrcics_list[key]:
+            for key in self.metrics_list:
+                for value in self.metrics_list[key]:
                     server_answer += f'{key} {value[0]} {value[1]}\n'
             return server_answer
         else:
-            if data_send[1] in self.metrcics_list.keys():
+            if data_send[1] in self.metrics_list.keys():
                 ser_answer = 'ok\n'
-                for value in self.metrcics_list[data_send[1]]:
-                    ser_answer += f'{value[0]} {value[1]}\n'
+                for value in self.metrics_list[data_send[1]]:
+                    ser_answer += f'{data_send[1]} {value[0]} {value[1]}\n'
                 return ser_answer
             else:
                 return 'there\nis\nno metric\n\n'
