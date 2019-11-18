8d61295c1499529ce7d7b741369d9fdacc21a11a
AntonKAb <anton_10_1999@mail.ru>
2019-11-18 10:24:52 +0700

    apdate

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-import asyncio
+
 import socket
 import time
 
@@ -97,66 +97,8 @@ class Client:
             raise ClientSocketError("error close connection", err)
 
 
-def run_server(host, port):
-    loop = asyncio.get_event_loop()
-    new_conn = loop.create_server(ClientServerProtocol, str(host), int(port))
-    server = loop.run_until_complete(new_conn)
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        print('Connection error\n\n')
-
-    server.close()
-    loop.run_until_complete(server.wait_closed())
-    loop.close()
-
-
-class ClientServerProtocol(asyncio.Protocol):
-    def __init__(self):
-        self.metrics_dict = {}
-
-    async def metrics_processing(self, data):
-        get_data = await data.read(1024)
-        try:
-            data_ = str(get_data.decode()).split()
-            if data_ == ['']:
-                print('error\nwrong command\n\n')
-            if data_[0] is 'put':
-                self.put_metric(data_)
-            if data_[0] is 'get':
-                self.get_metric(data_)
-            if data_[0] is not 'put' or 'get':
-                print('error\nwrong command\n\n')
-        except TimeoutError:
-            print('time\nis out\n\n')
-
-    def put_metric(self, data):
-        file = open('data.txt', 'a')
-        self.metrics_dict[data[1]] = data[2]
-        file.write(f'{data[1]} : {self.metrics_dict[data[1]]}')
-        file.close()
-        print('written\n\n')
-
-    def get_metric(self, data):
-        with open('data.txt', 'r') as file:
-            metrics = file.readlines()
-        list_ = []
-        if data[1] != '*':
-            for i in metrics:
-                if i.split()[0] == data[1]:
-                    list_.append(i.split()[0])
-                    print(i[:-1])
-            if len(list_) < 1:
-                print('there\nis\nno metric\n\n')
-        else:
-            for i in metrics:
-                if i.split()[0] == data[1]:
-                    print(i[:-1])
-
-
 def _main():
     # проверка работы клиента
-    run_server("127.0.0.1", 8888)
     client = Client("127.0.0.1", 8888, timeout=5)
     client.put("test", 0.5, timestamp=1)
     client.put("test", 2.0, timestamp=2)
